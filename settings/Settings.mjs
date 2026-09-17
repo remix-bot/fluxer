44fdf86962a4994595b8dd6193abb04709a0c01b
NoLogicAlan <corpsedev666@gmail.com>
2026-09-17 03:25:28 +0300

    now we can play Bilibili source Shanghai.

--- settings/Settings.mjs
+++
@@ -1,5 +0,0 @@
-/**
- * @module settings/Settings
- * @description Re-exports the core Settings classes from `src/Settings.mjs`.
- */
-export { SettingsManager, ServerSettings, RemoteSettingsManager } from "../src/db/Settings.mjs";
